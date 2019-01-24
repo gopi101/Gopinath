3a82808b3168c2648f1e3e8a40c02ebb14bca0d1
gopi101 <46590834+gopi101@users.noreply.github.com>
2019-01-24 11:55:27 +0530

    Update a character is vowel or consonant.py

--- a character is vowel or consonant.py
+++ a character is vowel or consonant.py
@@ -1,4 +1,4 @@
-ch = input("Enter a character: ")
+ch =z
 if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
 	    print(" Vowel")
 elif(ch=='$'or ch=='&'):
